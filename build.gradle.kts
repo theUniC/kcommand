7cd7f9c3284a865ac3eea6b770374b8f320d06d2
tλeUniC <318014+theUniC@users.noreply.github.com>
2024-04-14 16:27:45 +0200

    Publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import java.net.URI
+
 plugins {
     alias(libs.plugins.kotlin)
+    id("maven-publish")
 }
 
 allprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "maven-publish")
 
     repositories {
         mavenCentral()
@@ -15,5 +19,28 @@ allprojects {
 
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
+    }
+}
+
+subprojects {
+    publishing {
+        repositories {
+            maven {
+                name = "OSSRH"
+                url = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = System.getenv("MAVEN_USERNAME")
+                    password = System.getenv("MAVEN_PASSWORD")
+                }
+            }
+            maven {
+                name = "GitHubPackages"
+                url = URI("https://maven.pkg.github.com/theunic/kcommand")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
     }
 }
