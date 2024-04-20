f67bcb22acc37a0f2735c1e139911adf9dfeb79d
tλeUniC <318014+theUniC@users.noreply.github.com>
2024-04-20 12:46:07 +0200

    Try to fix publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin)
-    alias(libs.plugins.nmcp)
+    alias(libs.plugins.vanniktech)
 }
 
 allprojects {
@@ -11,6 +13,7 @@ allprojects {
 
 subprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "com.vanniktech.maven.publish")
 
     kotlin {
         jvmToolchain(21)
@@ -23,12 +26,40 @@ subprojects {
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
     }
+
+    mavenPublishing {
+        val artifactId = project.properties["name"].toString()
+        coordinates(rootProject.properties["groupId"].toString(), artifactId, rootProject.properties["version"].toString())
+
+        pom {
+            name.set(artifactId)
+            description.set(project.properties["description"].toString())
+            inceptionYear.set("2024")
+            url.set("https://github.com/theUniC/kcommand/")
+            licenses {
+                license {
+                    name.set("The MIT License")
+                    url.set("https://opensource.org/license/mit")
+                    distribution.set("https://opensource.org/license/mit")
+                }
+            }
+            developers {
+                developer {
+                    id.set("theUniC")
+                    name.set("Christian Soronellas")
+                    url.set("https://github.com/theUniC/")
+                }
+            }
+            scm {
+                url.set("https://github.com/theUniC/kcommand/")
+                connection.set("scm:git:git://github.com/theUniC/kcommand.git")
+                developerConnection.set("scm:git:ssh://git@github.com/theUniC/kcommand.git")
+            }
+        }
+    }
 }
 
-nmcp {
-    publishAllProjectsProbablyBreakingProjectIsolation {
-        username = System.getenv("CENTRAL_PORTAL_USERNAME")
-        password = System.getenv("CENTRAL_PORTAL_PASSWORD")
-        publicationType = "AUTOMATIC"
-    }
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.DEFAULT)
+    signAllPublications()
 }
