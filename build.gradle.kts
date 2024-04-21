0e76a3d88bacf9d0f0caa825f03574dca71f9eeb
tλeUniC <318014+theUniC@users.noreply.github.com>
2024-04-21 19:00:43 +0200

    Publish at jitpack.io

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
-import com.vanniktech.maven.publish.JavadocJar
-import com.vanniktech.maven.publish.KotlinJvm
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     alias(libs.plugins.kotlin)
-    alias(libs.plugins.nmcp)
-    alias(libs.plugins.vanniktech)
     alias(libs.plugins.dokka)
+    `maven-publish`
 }
 
 allprojects {
@@ -17,8 +12,7 @@ allprojects {
 
 subprojects {
     apply(plugin = "kotlin")
-    apply(plugin = "com.vanniktech.maven.publish")
-    apply(plugin = "com.gradleup.nmcp")
+    apply(plugin = "maven-publish")
     apply(plugin = "org.jetbrains.dokka")
 
     kotlin {
@@ -33,63 +27,14 @@ subprojects {
         useJUnitPlatform()
     }
 
-    nmcp {
-        publishAllPublications {
-            username = System.getenv("CENTRAL_PORTAL_USERNAME")
-            password = System.getenv("CENTRAL_PORTAL_PASSWORD")
-            publicationType = "AUTOMATIC"
-        }
-    }
-
-    mavenPublishing {
-        val artifactId = project.properties["name"].toString()
-
-        configure(
-            KotlinJvm(
-                javadocJar = JavadocJar.Dokka("dokkaHtml"),
-                sourcesJar = true,
-            ),
-        )
-
-        publishToMavenCentral(SonatypeHost.DEFAULT)
-        coordinates(rootProject.properties["groupId"].toString(), artifactId, rootProject.properties["version"].toString())
-
-        signAllPublications()
-
-        pom {
-            name.set(artifactId)
-            description.set(project.properties["description"].toString())
-            inceptionYear.set("2024")
-            url.set("https://github.com/theUniC/kcommand/")
-            licenses {
-                license {
-                    name.set("The MIT License")
-                    url.set("https://opensource.org/license/mit")
-                    distribution.set("https://opensource.org/license/mit")
-                }
-            }
-            developers {
-                developer {
-                    id.set("theUniC")
-                    name.set("Christian Soronellas")
-                    url.set("https://github.com/theUniC/")
-                }
-            }
-            scm {
-                url.set("https://github.com/theUniC/kcommand/")
-                connection.set("scm:git:git://github.com/theUniC/kcommand.git")
-                developerConnection.set("scm:git:ssh://git@github.com/theUniC/kcommand.git")
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["java"])
             }
         }
-    }
-}
-
-nmcp {
-    publishAggregation {
-        project(":kcommand-core")
-
-        username = System.getenv("CENTRAL_PORTAL_USERNAME")
-        password = System.getenv("CENTRAL_PORTAL_PASSWORD")
-        publicationType = "AUTOMATIC"
     }
 }
